e14bc7f4afdc498a63bc8df1faac18b0156090af
DanI4E <che25d@mail.ru>
2024-02-18 16:35:43 +0700

    добавление ConfigDict в схемы

--- schemas.py
+++ schemas.py
@@ -1,5 +1,5 @@
 from typing import Optional # Optional дает возможность не заполнять данные в параметре, по умолчанию None
-from pydantic import BaseModel # для описание данных, валидация (устанавливается вместе в fastapi)
+from pydantic import BaseModel, ConfigDict # для описание данных, валидация (устанавливается вместе в fastapi)
 
 
 class STaskAdd(BaseModel): # класс от которого наследуются все наши модели
@@ -10,6 +10,8 @@ class STaskAdd(BaseModel): # класс от которого наследуют
 
 class STask(STaskAdd): # наследование параметров из класса STaskAdd(BaseModel)
     id: int
+
+    model_config = ConfigDict(from_attributes=True) # парсит не только как словарь данный объект, а еще как экземпляр класса и из атрибутов его достаёт все свойства
 
 
 
